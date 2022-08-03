6b9189a71a1a1f04dfc4670bb7a8c92dfda1e251
Kevin Palowa <kevinpalowa@gmail.com>
2022-08-03 13:39:33 +0700

    Add NavBar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,21 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import React from "react";
+import NavBar from "../components/NavBar";
 interface Props {
   children: React.ReactNode;
   title: string;
 }
 const Layout: NextPage<Props> = ({ children, title }) => {
   return (
-    <div>
+    <div className="bg-dark">
       <Head>
         <title>{title}</title>
       </Head>
-      {children}
+      <nav>
+        <NavBar />
+      </nav>
+      <main>{children}</main>
     </div>
   );
 };
---
+++ components/NavBar.tsx
@@ -0,0 +1,34 @@
+import { NextPage } from "next";
+import Link from "next/link";
+import React from "react";
+import { NextRouter, useRouter } from "next/router";
+const NavLink = ({ children, href }: { children: string; href: string }) => {
+  const router: NextRouter = useRouter();
+  const isActive = router.asPath === href;
+  console.log(router.pathname);
+  return (
+    <Link href={href} className="bg-blue-100">
+      <a
+        className={`text-lg hover:text-white ${
+          isActive ? "text-white" : "text-gray-400"
+        }`}
+      >
+        {children}
+      </a>
+    </Link>
+  );
+};
+const NavBar: NextPage = () => {
+  return (
+    <div className="bg-[#2c3440] py-3 text-white flex justify-between">
+      <h2 className="text-4xl font-bold">Movies</h2>
+      <ul className="space-x-3 flex items-center">
+        <NavLink href="/movies">Movies</NavLink>
+        <NavLink href="/popular">Popular</NavLink>
+        <NavLink href="/popular">Account</NavLink>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
