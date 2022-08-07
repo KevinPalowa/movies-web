24c15c755c4bc787f4bf257926b15ef6c9ca2b7a
Kevin Palowa <kevinpalowa@gmail.com>
2022-08-08 00:15:57 +0700

    Add InfoTab

---
+++ components/InfoTab.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import React, { useState } from "react";
+
+const InfoTab = ({ data }) => {
+  const [activeTab, setActiveTab] = useState("cast");
+  return (
+    <div>
+      <div className="flex space-x-5">
+        <p
+          className={`
+            ${
+              activeTab === "cast" && "text-green-500 border-b border-white"
+            } cursor-pointer`}
+          onClick={() => setActiveTab("cast")}
+        >
+          Cast
+        </p>
+        <p
+          className={`${
+            activeTab === "genres" && "text-green-500 border-b border-white"
+          } cursor-pointer`}
+          onClick={() => setActiveTab("genres")}
+        >
+          Genres
+        </p>
+      </div>
+      <div className="flex mt-3 w-full flex-wrap">
+        {activeTab === "cast"
+          ? data.credits.cast.map((cast) => (
+              <Link key={cast.id} href={`/cast/${cast.id}`}>
+                <a className="mr-1 mb-0.5 my-0.5 block text-xs bg-gray-800  p-1 items-center whitespace-nowrap">
+                  {cast.name}
+                </a>
+              </Link>
+            ))
+          : data.genres.map((genre) => (
+              <Link key={genre.id} href={`/genre/${genre.id}`}>
+                <a className="mr-1 my-0.5 block text-xs bg-gray-800  p-1 items-center whitespace-nowrap">
+                  {genre.name}
+                </a>
+              </Link>
+            ))}
+      </div>
+    </div>
+  );
+};
+
+export default InfoTab;
--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -3,7 +3,7 @@ import { GetServerSideProps, NextPage } from "next";
 import React from "react";
 import Layout from "../../components/Layout";
 import PosterImage from "../../components/image/PosterImage";
-import Link from "next/link";
+import InfoTab from "../../components/InfoTab";
 
 type Props = {
   data: {
@@ -17,11 +17,6 @@ type Props = {
 };
 const Movie: NextPage<Props> = ({ data }) => {
   console.log(data);
-  // const { id } = router.query;
-  // const [data, setData] = useState<Data>();
-  // useEffect(() => {
-  //   axios.get(`/api/movie/${id}`).then((res) => setData(res.data));
-  // }, [id]);
   return (
     <Layout title={data.title}>
       <div className="flex space-x-10">
@@ -39,29 +34,18 @@ const Movie: NextPage<Props> = ({ data }) => {
             </span>
           </p>
           <p className="mt-5 text-gray-400">{data?.overview}</p>
-          <div>
-            <p>Cast: </p>
-            <div className="flex space-y-1 w-full flex-wrap">
-              {data.credits.cast.map((cast) => (
-                <Link key={cast.id} href={`/cast/${cast.id}`}>
-                  <a className="mr-1 my-0.5 block text-xs bg-gray-800  p-1 items-center whitespace-nowrap">
-                    {cast.name}
-                  </a>
-                </Link>
-              ))}
-            </div>
-          </div>
+          <InfoTab data={data} />
         </div>
       </div>
     </Layout>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   // const router = useRouter();
   // const { id } = router.query;
   const res = await axios.get(
-    `https://api.themoviedb.org/3/movie/616037?api_key=${process.env.API_KEY}&language=en-US&append_to_response=credits`
+    `https://api.themoviedb.org/3/movie/${query.id}?api_key=${process.env.API_KEY}&language=en-US&append_to_response=credits`
   );
   return {
     props: { data: res.data },
