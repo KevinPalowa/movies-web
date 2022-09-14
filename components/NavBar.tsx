8ae5b40f5856b5acf1be3134a7a7b9230a496c82
Kevin Palowa <kevinpalowa@gmail.com>
2022-09-15 01:11:46 +0700

    Remove auth0

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import { NextRouter, useRouter } from "next/router";
-import { useUser } from "@auth0/nextjs-auth0";
-import { FiLogOut } from "react-icons/fi";
 const NavLink = ({ children, href }: { children: string; href: string }) => {
   const router: NextRouter = useRouter();
   const isActive = router.asPath === href;
@@ -20,40 +17,7 @@ const NavLink = ({ children, href }: { children: string; href: string }) => {
   );
 };
 
-const Modal = ({ user, isActive }: any) => {
-  return (
-    <div
-      className={`absolute top-14 z-10 p-5 rounded-lg right-56 bg-[#2c3440] border border-gray-500 ${
-        !isActive && "hidden"
-      }`}
-    >
-      <div className="flex space-x-1 items-center text-sm text-gray-400">
-        <div className="w-8 h-8 relative ">
-          <Image
-            className="rounded-full"
-            src={user.picture}
-            alt="profile picture"
-            layout="fill"
-          />
-        </div>
-        <div className="flex-row">
-          <p className="text-white">{user.name}</p>
-          <p>{user.email}</p>
-        </div>
-      </div>
-      <div className="flex items-center space-x-1">
-        <FiLogOut />
-        <Link href="/api/auth/logout">
-          <a className="text-sm text-inherit">Log out</a>
-        </Link>
-      </div>
-    </div>
-  );
-};
-
 const NavBar = () => {
-  const { user } = useUser();
-  const [isActive, setIsActive] = useState(false);
   return (
     <div className="bg-[#2c3440] py-3">
       <div className="max-w-4xl w-full flex justify-between mx-auto">
@@ -63,31 +27,6 @@ const NavBar = () => {
         <ul className="space-x-3 flex items-center">
           <NavLink href="/movies">Movies</NavLink>
           <NavLink href="/popular">Popular</NavLink>
-          {user ? (
-            <>
-              <div
-                onClick={() => setIsActive(!isActive)}
-                className="flex items-center space-x-1 cursor-pointer"
-              >
-                <div className="w-8 h-8 relative ">
-                  {typeof user.picture === "string" ? (
-                    <Image
-                      className="rounded-full"
-                      src={user.picture}
-                      alt="profile picture"
-                      layout="fill"
-                    />
-                  ) : (
-                    ""
-                  )}
-                </div>
-              </div>
-              <Modal isActive={isActive} user={user} />
-              {/* <NavLink href="/api/auth/logout">Logout</NavLink> */}
-            </>
-          ) : (
-            <NavLink href="/api/auth/login">Login</NavLink>
-          )}
         </ul>
       </div>
     </div>
