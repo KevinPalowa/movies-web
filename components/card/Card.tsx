bd521f9c7230818dd642442320f6d15dac83ef5a
Kevin Palowa <kevinpalowa@gmail.com>
2022-10-18 21:37:58 +0700

    fix card overlay

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -10,37 +10,39 @@ type Props = {
   id: number | string;
 };
 const Card: NextPage<Props> = ({ id, src, title }) => {
-  const hoverRef = useRef<HTMLAnchorElement>(null);
+  const hoverRef = useRef<HTMLDivElement>(null);
   const isHover = useHover(hoverRef);
   return (
-    <Link href={`/movie/${id}`}>
-      <a
-        ref={hoverRef}
-        className="cursor-pointer h-56 rounded-md relative border-2 border-gray-500 transition hover:border-green-400 hover:border-[2px]"
-      >
-        {src !== null ? (
-          <Image
-            className="rounded-md"
-            objectFit="cover"
-            layout="fill"
-            src={`https://image.tmdb.org/t/p/w185${src}`}
-            alt={title}
-          />
-        ) : (
-          <div className="bg-gray-800 w-full h-full font-bold rounded-md  text-center items-center flex justify-center">
-            {title}
-          </div>
-        )}
-        {isHover && (
-          <>
-            <div className="absolute -top-5 bg-gray-700 text-xs px-2 rounded-sm font-bold whitespace-nowrap">
+    <div
+      ref={hoverRef}
+      className="cursor-pointer h-56 rounded-md relative border-2 border-gray-500 transition hover:border-green-400 hover:border-[2px]"
+    >
+      <Link href={`/movie/${id}`}>
+        <a>
+          {src !== null ? (
+            <Image
+              className="rounded-md"
+              objectFit="cover"
+              layout="fill"
+              src={`https://image.tmdb.org/t/p/w185${src}`}
+              alt={title}
+            />
+          ) : (
+            <div className="bg-gray-800 w-full h-full font-bold rounded-md  text-center items-center flex justify-center">
               {title}
             </div>
-            <Overlay />
-          </>
-        )}
-      </a>
-    </Link>
+          )}
+        </a>
+      </Link>
+      {isHover && (
+        <>
+          <div className="absolute -top-5 bg-gray-700 text-xs px-2 rounded-sm font-bold whitespace-nowrap">
+            {title}
+          </div>
+          <Overlay />
+        </>
+      )}
+    </div>
   );
 };
 
